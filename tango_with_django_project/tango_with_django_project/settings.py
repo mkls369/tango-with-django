8eaf447e887e1c8879c7269927e9e97c81ac7570
xerlivex <mykolas.sum@gmail.com>
2015-11-02 21:54:23 +0200

    Addedstatic root5

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -77,7 +77,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT= os.path.join(PROJECT_DIR,'static/')
+STATIC_ROOT= "/home/xerlivex/tango_with_django/tango_with_django_project/static"
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"

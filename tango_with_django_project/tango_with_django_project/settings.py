51b24c35ed37fd2644a4e6632df64fa384bc8deb
xerlivex <mykolas.sum@gmail.com>
2015-11-02 21:36:07 +0200

    Addedstatic root3

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -6,6 +6,7 @@ SETTINGS_DIR = os.path.dirname(__file__)
 
 PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
 PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+PROJECT_DIR=os.path.dirname(__file__)
 
 TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
@@ -76,7 +77,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT= os.path.join(PROJECT_DIR,'static_media/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
